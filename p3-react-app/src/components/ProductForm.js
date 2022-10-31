c53f49ca8d4ea4a3fe08f8937f74d1d2174a20e5
jrosi <j.rosimo098@gmail.com>
2022-10-31 12:30:38 +0800

    Feat: added css to product form

--- p3-react-app/src/components/ProductForm.js
+++ p3-react-app/src/components/ProductForm.js
@@ -4,6 +4,9 @@ import { useState } from "react";
 //Import redux
 import { useDispatch, useSelector } from "react-redux";
 
+//Import css
+import "./ProductForm.css";
+
 const ProductForm = () => {
   //Create variable and function to add each input to the initial state using useState hook
   const [name, setName] = useState("");
@@ -56,56 +59,60 @@ const ProductForm = () => {
   };
 
   return (
-    <form className="ProductForm" onSubmit={onAddProductHandler}>
-      <div>
-        <label htmlFor="product-name">Product Name:</label>
-        <input
-          id="product-name"
-          type="text"
-          value={name}
-          placeholder="Vegetable name"
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="kilogram">Kilograms:</label>
-        <input
-          id="kilogram"
-          type="number"
-          value={kilo}
-          placeholder="Enter estimated kilo"
-          onChange={(e) => Number(setKilo(e.target.value))}
-        />
-      </div>
-      <div>
-        <label htmlFor="producer">Producer's name:</label>
-        <input
-          id="producer"
-          type="text"
-          value={producer}
-          placeholder="Enter your name/group"
-          onChange={(e) => setProducer(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="datePlanted">Date planted:</label>
-        <input
-          id="datePlanted"
-          type="date"
-          value={datePlanted}
-          onChange={(e) => setDatePlanted(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="date">Havest date &#40;target&#41;:</label>
-        <input
-          id="date"
-          type="date"
-          value={dateOfHarvest}
-          onChange={(e) => setDateOfHarvest(e.target.value)}
-        />
-      </div>
-      <div>
+    <>
+      <form className="product-form" onSubmit={onAddProductHandler}>
+        <h2>Add vegetable crop</h2>
+        <div>
+          <label className="label" htmlFor="product-name">
+            Product Name:
+          </label>
+          <input
+            id="product-name"
+            type="text"
+            value={name}
+            placeholder="Vegetable name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="kilogram">Kilograms:</label>
+          <input
+            id="kilogram"
+            type="number"
+            value={kilo}
+            placeholder="Enter estimated kilo"
+            onChange={(e) => Number(setKilo(e.target.value))}
+          />
+        </div>
+        <div>
+          <label htmlFor="producer">Producer's name:</label>
+          <input
+            id="producer"
+            type="text"
+            value={producer}
+            placeholder="Enter your name/group"
+            onChange={(e) => setProducer(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="datePlanted">Date planted:</label>
+          <input
+            id="datePlanted"
+            type="date"
+            value={datePlanted}
+            onChange={(e) => setDatePlanted(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="date">Havest date &#40;target&#41;:</label>
+          <input
+            id="date"
+            type="date"
+            value={dateOfHarvest}
+            onChange={(e) => setDateOfHarvest(e.target.value)}
+          />
+        </div>
+        {/* <div>
         <label htmlFor="image">Image url:</label>
         <input
           id="image"
@@ -113,10 +120,11 @@ const ProductForm = () => {
           placeholder="Enter image URL"
           onChange={(e) => setImage(e.target.value)}
         />
-      </div>
-      {hasError && <small className="error-msg">{msgError}</small>}
-      <button type="submit">Add Product</button>
-    </form>
+      </div> */}
+        {hasError && <small className="error-msg">{msgError}</small>}
+        <button type="submit">Add Product</button>
+      </form>
+    </>
   );
 };
 
