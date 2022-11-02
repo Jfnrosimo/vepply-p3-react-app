fe7a32109d67d5aaf9fa7ee515d341f4903a0007
jrosi <j.rosimo098@gmail.com>
2022-11-02 15:10:05 +0800

    Fix: table page to view 10 items per page

--- p3-react-app/src/pages/ProductListsPage.js
+++ p3-react-app/src/pages/ProductListsPage.js
@@ -21,13 +21,12 @@ const ProductListsPage = () => {
   const dispatch = useDispatch();
 
   //Create pagination for table
-
   const allProductPageData = useMemo(
-    () => productData.slice(page * 5, page * 5 + 5),
+    () => productData.slice(page * 10, page * 10 + 10),
     [page]
   );
-  console.log(allProductPageData);
 
+  console.log(allProductPageData);
   console.log(harvestTable);
 
   return (
@@ -35,47 +34,50 @@ const ProductListsPage = () => {
       <div className="product-list-container">
         <ProductForm />
 
-        <table>
-          <thead>
-            <tr>
-              <th colSpan={5}>List of All Products</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="table-row-heading">
-              <th>Name</th>
-              <th>Kilo</th>
-              <th>Date Planted</th>
-              <th>Harvest Date</th>
-              <th>Producer</th>
-            </tr>
-            {allProductPageData.map((item) => (
-              <ProductRow
-                key={item.id}
-                id={item.id}
-                name={item.name}
-                kilogram={item.kilogram}
-                datePlanted={item.datePlanted}
-                dateOfHarvest={item.dateOfHarvest}
-                producer={item.producer}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div className="table-button-container">
-        <button onClick={() => dispatch({ type: "PREVIOUS_PAGE" })}>
-          Previous page
-        </button>
-        <button
-          onClick={
-            allProductPageData.length !== 0
-              ? () => dispatch({ type: "NEXT_PAGE" })
-              : null
-          }
-        >
-          Next page
-        </button>
+        <div className="table-1-container">
+          <table>
+            <thead>
+              <tr>
+                <th colSpan={5}>List of All Products</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="table-row-heading">
+                <th>Name</th>
+                <th>Kilo</th>
+                <th>Date Planted</th>
+                <th>Harvest Date</th>
+                <th>Producer</th>
+              </tr>
+              {allProductPageData.map((item) => (
+                <ProductRow
+                  key={item.id}
+                  id={item.id}
+                  name={item.name}
+                  kilogram={item.kilogram}
+                  datePlanted={item.datePlanted}
+                  dateOfHarvest={item.dateOfHarvest}
+                  producer={item.producer}
+                />
+              ))}
+            </tbody>
+          </table>
+
+          <div className="table-button-container">
+            <button onClick={() => dispatch({ type: "PREVIOUS_PAGE" })}>
+              Previous page
+            </button>
+            <button
+              onClick={
+                allProductPageData.length !== 0
+                  ? () => dispatch({ type: "NEXT_PAGE" })
+                  : null
+              }
+            >
+              Next page
+            </button>
+          </div>
+        </div>
       </div>
 
       <div className="product-list-2-container" id="harvested-crop-table">
@@ -85,6 +87,7 @@ const ProductListsPage = () => {
               <th colSpan={5}>Your harvested crops</th>
             </tr>
           </thead>
+
           <tbody>
             <tr className="table-row-heading">
               <th>Name</th>
